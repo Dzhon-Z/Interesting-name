22ae64bdda9d699bd8b00b4e0ca4bc9d5e63fa53
Dzhon <evgeniy1203or@gmail.com>
2024-11-25 21:43:03 +0300

    Update Task 1.py

--- Lab 1/Task 1.py
+++ Lab 1/Task 1.py
@@ -2,6 +2,10 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 # TODO заменить значение пропущенного элемента средним арифметическим
 
-sr = (sum(numbers[0:4])+sum(numbers[5:]))/len(numbers)
+for i in range(len(numbers)):
+    if numbers[i] is None:
+        miss: int = i
+
+sr = (sum(numbers[0:miss])+sum(numbers[5:]))/len(numbers)
 numbers[4] = sr
 print("Измененный список:", numbers)
