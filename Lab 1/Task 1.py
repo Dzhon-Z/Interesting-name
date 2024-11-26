d9c12f618df41305933720d55e73dc9a113032a0
Dzhon <evgeniy1203or@gmail.com>
2024-11-26 09:54:54 +0300

    Update Task 1.py

--- Lab 1/Task 1.py
+++ Lab 1/Task 1.py
@@ -4,8 +4,8 @@ numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -
 
 for i in range(len(numbers)):
     if numbers[i] is None:
-        miss: int = i
+        miss = i
 
 sr = (sum(numbers[0:miss])+sum(numbers[5:]))/len(numbers)
-numbers[4] = sr
+numbers[miss] = sr
 print("Измененный список:", numbers)
